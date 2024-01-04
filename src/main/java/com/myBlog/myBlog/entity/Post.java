7cff7d9f409d7fcb1c5d24455c932d141fa044e0
Jabir <saifalikhan8828@gmail.com>
2024-01-04 11:16:16 +0530

    127:Create Save Post Feature

---
+++ src/main/java/com/myBlog/myBlog/entity/Post.java
@@ -0,0 +1,22 @@
+package com.myBlog.myBlog.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+}
